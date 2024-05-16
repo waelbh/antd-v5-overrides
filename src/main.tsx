de4fa09472f8316eaa490d788fee2ee66d667e8e
waelbh <wael.benhadjmhamed@esprit.tn>
2024-05-16 22:06:07 +0100

    test theme overrides

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './demo';
 import './index.less';
+import { ConfigProvider } from 'antd';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+     <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: "'Work Sans', sans-serif",
+        }
+      }}
+    >
     <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
